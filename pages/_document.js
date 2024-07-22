323b70663dcfa58847e5870a9e889e85a96a579d
6odevi7 <djtaoist@gmail.com>
2024-07-21 18:56:19 -0800

    Version 1.0.2-5

--- pages/_document.js
+++ pages/_document.js
@@ -94,32 +94,6 @@ const LoginForm = () => {
 };
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
   render() {
     return (
       <Html>
@@ -151,3 +125,4 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
